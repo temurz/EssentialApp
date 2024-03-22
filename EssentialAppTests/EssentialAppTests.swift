4ccc514232e82b2d2039b5f78507896a9e68cdca
Temur <timurzaitov1998@gmail.com>
2024-03-22 16:42:54 +0100

    Extract system under test (SUT) creation into a factory method

--- EssentialAppTests/EssentialAppTests.swift
+++ EssentialAppTests/EssentialAppTests.swift
@@ -20,13 +20,11 @@ class FeedLoaderWithFallbackComposite: FeedLoader {
 }
 
 final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
-
+    
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
-        let primaryLoader = LoaderStub(result: .success(primaryFeed))
-        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
-        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
         let exp = expectation(description: "Wait for load completion")
         sut.load { result in
             switch result {
@@ -40,6 +38,24 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
         
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
+        let primaryLoader = LoaderStub(result: primaryResult)
+        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        trackForMemoryLeaks(primaryLoader)
+        trackForMemoryLeaks(fallbackLoader)
+        trackForMemoryLeaks(sut)
+        return sut
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line ) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
+        }
     }
     
     private func uniqueFeed() -> [FeedImage] {
