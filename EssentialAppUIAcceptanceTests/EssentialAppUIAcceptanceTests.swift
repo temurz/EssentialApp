b8f942d557d005983095b4f3fe5e091f415838f1
Temur <timurzaitov1998@gmail.com>
2024-04-02 18:08:48 +0200

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Temur on 02/04/2024.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+    
+}
