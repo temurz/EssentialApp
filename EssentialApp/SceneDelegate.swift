be2f00db78e8934a92e93f28fc29201491acbcb1
Temur <timurzaitov1998@gmail.com>
2024-08-20 23:30:11 +0500

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -67,8 +67,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-        let 
-        remoteURL = URL(string:"https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential _app_feed.json")!
+//        let 
+//        remoteURL = URL(string:"https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential _app_feed.json")!
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
         
         return remoteFeedLoader
